2ed517793e27e33b905b9e59c19b633a407c81de
adria <adria@DESKTOP-Q4P4BOO>
2020-10-12 13:04:56 +0200

    Ejercicio4

---
+++ Ejercicio4/Ejercicio4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Ejercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            
+
+            bool esPar = false;
+
+            Console.WriteLine("Escriba dos números enteros para ver si son pares o no:");
+            Console.WriteLine("Primer número:");
+            string line;
+            line = Console.ReadLine();
+            x = int.Parse(line);
+            Console.WriteLine("Segundo número:");
+            line = Console.ReadLine();
+            y = int.Parse(line);
+
+            //compromabos para x si es par
+            if((x % 2) == 0)
+            {
+                esPar = true;
+                Console.WriteLine("El número " + x + " es Par");
+            }
+            else if ((x % 2) != 0)
+            {
+                esPar = false;
+                Console.WriteLine("El número " + x + " es Impar");
+            }
+
+            //compromabos para y si es par
+            if ((y % 2) == 0)
+            {
+                esPar = true;
+                Console.WriteLine("El número " + y + " es Par");
+            }
+            else if ((y % 2) != 0)
+            {
+                esPar = false;
+                Console.WriteLine("El número " + y + " es Impar");
+            }
+
+
+        }
+    }
+}
