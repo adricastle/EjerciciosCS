7bdccf415aa05cbfed2b9db59605cabc81e97cb2
adria <adria@DESKTOP-Q4P4BOO>
2020-10-16 20:46:58 +0200

    Ejercicios 9 y 10

---
+++ Ejercicio10/Ejercicio10/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+
+namespace Ejercicio10
+{
+    class Alumno
+    {
+        private string nombre;
+        private int edad;
+
+
+        public Alumno(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        public Alumno()
+        {
+        }
+
+        public void setNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public string getNombre()
+        {
+            return this.nombre;
+        }
+
+        public void setEdad(int edad)
+        {
+            
+            this.edad = edad;
+
+        }
+
+        public int getEdad()
+        {
+            return this.edad;
+        }
+
+        public void printAlumno(int i)
+        {
+            Console.WriteLine("Alumno " + (i+1));
+            Console.WriteLine("Nombre: " + this.getNombre());
+            Console.WriteLine("Edad: " + this.getEdad());
+        }
+        static void Main(string[] args)
+        {
+            int totalAlumnos = 0;
+            Alumno[] listaAlumnos = new Alumno[5];
+
+            
+            bool exit = false;
+
+            Console.WriteLine("Hola, introduce nombre y edad de 5 alumnos y se finaliza cuando se introduzca un '*'");
+            
+            for (int i = 0; (i<5) && (exit == false); i++)
+            {
+                Alumno alumno = new Alumno();
+
+                Console.WriteLine("\nIntrodce el Nombre del alumno " + (i+1));
+                string line;
+                line = Console.ReadLine();
+                alumno.setNombre(line);
+                if(line == "*")
+                {
+                    exit = true;
+                }
+                Console.WriteLine("Introdce la edad del alumno " + (i+1));
+                
+                line = Console.ReadLine();
+                alumno.setEdad(int.Parse(line));
+                listaAlumnos[i] = alumno;
+
+                totalAlumnos = i;
+            }
+            
+
+            
+
+
+            Console.WriteLine("Los Alumnos mayores de edad son los siguientes: ");
+            Alumno mayor = new Alumno();
+            int numeroMayor = 0;
+            for (int i = 0; i < totalAlumnos; i++)
+            {
+                if(listaAlumnos[i].getEdad() >= 18)
+                {
+                    listaAlumnos[i].printAlumno(i);
+                }
+
+                if(listaAlumnos[i].getEdad() > mayor.getEdad())
+                {
+                    mayor = listaAlumnos[i];
+                    numeroMayor = i;
+                }
+                
+            }
+
+            Console.WriteLine("\nEl Alumno más viejo es: ");
+            mayor.printAlumno(numeroMayor);
+
+
+        }
+
+        
+    }
+}
---
+++ Ejercicio9/Ejercicio9/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace Ejercicio9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            double division = 0;
+
+            Console.WriteLine("Hola, Introduce dos numeros enteros a y b para dividir a entre b:");
+            try
+            {
+                Console.WriteLine("Primer número");
+                string line = Console.ReadLine();
+                a = int.Parse(line);
+
+                Console.WriteLine("Segundo número");
+                line = Console.ReadLine();
+                b = int.Parse(line);
+
+                division = a / b;
+
+                Console.WriteLine("La división de " + a + " entre " + b  + " es:" + division);
+
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("el valor debe ser un entero");
+          
+            }
+            catch (OverflowException o)
+            {
+                Console.WriteLine("no puede ser un numero muy grande");
+            }
+            catch (DivideByZeroException d)
+            {
+
+                Console.WriteLine("divisor no puede ser > 0 ");
+            }
+            
+
+        }
+    }
+}
